6d8d5499a2edf303c443f27606b463d45ebd9b34
tulare <tulare.paxgalactica@gmail.com>
2018-11-12 14:45:00 +0100

    Refactoring package sources into src and adding tests

--- setup.py
+++ setup.py
@@ -1,30 +1,15 @@
 # -*- encoding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
-import os
-import sys
-from pkg_resources import Requirement
-from setuptools import setup, find_packages
+import setuptools
 
 # Get version without import module
-exec(compile(open('pk_services/version.py').read(),
+exec(compile(open('src/pk_services/version.py').read(),
              'pk_services/version.py', 'exec'))
 
-with open('README.md') as f :
-    readme = f.read()
-
-with open('LICENSE') as f :
-    license = f.read()
-
-setup(
-    name='pk-services',
-    version=__version__,
-    description='Various services for python projects',
-    long_description=readme,
-    author='Tulare Regnus',
-    author_email='tulare.paxgalactica@gmail.com',
-    url='https://github.com/tulare/pk-services',
-    license=license,
-    packages=find_packages(exclude=('tests',)),
-    zip_safe=False,
-    #install_requires=[defined in setup.cfg],
+setuptools.setup(
+    version = __version__,
+    package_dir = {
+        '' : str('src')
+    },
 )
---
+++ tests/test_pk_services.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
+import unittest
+import pk_services
+
+class Test_global(unittest.TestCase) :
+
+    def setUp(self) :
+        pass
+    
+    def tearDown(self):
+        pass
+
+    def test_A(self) :
+        assert True, 'this test'
+        
+if __name__ == '__main__' :
+    unittest.main()
