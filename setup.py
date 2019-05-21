235b80c78ac198a76c9be9e3c04b4e7cb8c7baa0
tulare <tulare.paxgalactica@gmail.com>
2019-05-21 17:39:41 +0200

    fix license setup

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from __future__ import absolute_import, unicode_literals
 
 import setuptools
 
+with open('LICENSE') as f:
+    license = f.read()
+
 # Get version without import module
 exec(compile(open('src/pk_services/version.py').read(),
              'pk_services/version.py', 'exec'))
 
 setuptools.setup(
     version = __version__,
+    license=license,
     package_dir = {
         '' : str('src')
     },
